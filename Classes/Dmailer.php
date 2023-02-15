456b764478f0d8c7b574565f6ecb367b3d6960fd
Christoph Lehmann <post@christophlehmann.eu>
2023-02-15 14:47:21 +0100

    Use original filename for attachments
    Without this attachments have names like `fal-tempfile...pdf`

--- Classes/Dmailer.php
+++ Classes/Dmailer.php
@@ -889,11 +889,9 @@ class Dmailer implements LoggerAwareInterface
         // handle FAL attachments
         if ((int)$this->dmailer['sys_dmail_rec']['attachment'] > 0) {
             $files = DirectMailUtility::getAttachments((int)$this->dmailer['sys_dmail_rec']['uid']);
-            if(count($files)) {
-                /** @var FileReference $file */
-                foreach ($files as $file) {
-                    $mailer->attachFromPath($file->getForLocalProcessing());
-                }
+
+            foreach ($files as $file) {
+                $mailer->attachFromPath($file->getForLocalProcessing(), $file->getName());
             }
         }
     }
